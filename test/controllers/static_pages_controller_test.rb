3c65a7d1457d15bb97b1a3fcdaa7136b512c06a3
Richard Harris <richardharris118@gmail.com>
2020-11-17 20:02:34 +0000

    Static pages started with dynamic titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", {count: 1, text: "Home | Shepley Computers"}, "Wrong or more than one title" 
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", {count: 1, text: "About | Shepley Computers"}, "Wrong or more than one title"    
   end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", {count: 1, text: "Contact | Shepley Computers"}, "Wrong or more than one title" 
+  end
 end
