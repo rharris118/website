cc3d778ef91dd6f25eab791181ce0931b0240e4b
Richard Harris <richardharris118@gmail.com>
2020-12-14 22:10:56 +0000

    comment out hasmany/belongs to

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   validates_presence_of :title
 
   #has_many :product_categories
-  has_many :categories, through: product_categories
+  #has_many :categories, through: product_categories
 end
