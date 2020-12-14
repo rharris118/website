b0a2798f8158cf337b0f7345a3ef069175e10c26
Richard Harris <richardharris118@gmail.com>
2020-12-14 17:20:47 +0000

    edit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates_presence_of :title
 
-  has_many :product_categories
+  #has_many :product_categories
   has_many :categories, through: product_categories
 end
